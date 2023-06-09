3c8ba978ec04276996c7b24d95a8711c395b5d9f
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-06-09 09:44:17 +0600

    Added NavBar

--- react-multipage/src/App.js
+++ react-multipage/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <div>
-      Hello World
+      <NavBar/>
     </div>
   );
 }
