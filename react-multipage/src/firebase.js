7881d386b323d8a99c721682b54cfd01df289a1d
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-06-16 13:51:03 +0600

    Update firebase

--- react-multipage/src/firebase.js
+++ react-multipage/src/firebase.js
@@ -1,5 +1,6 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyBAZkNR2E8Bfru3LCThrELuoCGYTuVcCd4",
     authDomain: "projectsync-98cb4.firebaseapp.com",
     projectId: "projectsync-98cb4",
@@ -7,14 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "224430306473",
     appId: "1:224430306473:web:8823a5715d6856d39ea5b8",
     measurementId: "G-CJ80ZJECCT"
-};
-
-import firebase from "firebase";
-const firebaseApp = firebase.initializeApp({
-
 });
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
-const stroagh
+const stroage = firebase.storage();
+
 export default db;
