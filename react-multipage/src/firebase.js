d63d0face7909ae404ba760ebf4e00d2be19f6f9
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-06-16 13:46:20 +0600

    Create firebase.js

---
+++ react-multipage/src/firebase.js
@@ -0,0 +1,20 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBAZkNR2E8Bfru3LCThrELuoCGYTuVcCd4",
+    authDomain: "projectsync-98cb4.firebaseapp.com",
+    projectId: "projectsync-98cb4",
+    storageBucket: "projectsync-98cb4.appspot.com",
+    messagingSenderId: "224430306473",
+    appId: "1:224430306473:web:8823a5715d6856d39ea5b8",
+    measurementId: "G-CJ80ZJECCT"
+};
+
+import firebase from "firebase";
+const firebaseApp = firebase.initializeApp({
+
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const stroagh
+export default db;
